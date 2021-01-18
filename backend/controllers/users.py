a8493adaa24248d4b3aa88fc3d2ba80671fa1aa4
msknkh <muskankhandelwal369@gmail.com>
2021-01-18 13:17:01 +0530

    User update and get routes done
    Signed-off-by: msknkh <muskankhandelwal369@gmail.com>

--- backend/controllers/users.py
+++ backend/controllers/users.py
@@ -7,6 +7,11 @@ def createUser(data):
     try :
         user = User(username = data['username'], email = data['email'], 
                     password = data['password']) 
+        if "image" in data: 
+            user.image = data['image']
+        if "bio" in data: 
+            user.bio = data['bio']
+        print(user)
         user.token = create_access_token(identity=user.username)
         db.session.add(user)
         db.session.commit()
@@ -19,11 +24,10 @@ def createUser(data):
             raise Exception("No email provided")
         if 'password' not in data:
             raise Exception("No password provided")
-        if User.query.filter_by(username = data['username']) != None:
+        if User.query.filter_by(username = data['username']) is not None:
             raise Exception("User already exists")
 
         raise Exception("Could not add the user to the database")
-
 
 def loginUser(data):
 
@@ -45,6 +49,37 @@ def loginUser(data):
     user.token = create_access_token(identity = user.username, fresh=False)
     return user.serialize()
 
+def getUserByUsername(username):
+    try:
+        user = User.query.filter_by(username = username).first()
+        return user.serialize()
+    
+    except Exception as e:
+        if User.query.filter_by(username = data['username']) is None:
+            raise Exception("User does not exists")
 
+        raise Exception(str(e))
 
-        
+def updateUser(username, data):
+    try:
+        user = User.query.filter_by(username = username).first()
+        if "email" in data: 
+            user.email = data['email']
+        if "username" in data: 
+            user.username = data['username']
+        if "password" in data: 
+            user.set_password(data['password'])
+        if "image" in data: 
+            user.image = data['image']
+        if "bio" in data: 
+            user.bio = data['bio']
+            
+        user.token = create_access_token(identity = user.username, fresh=False)
+        db.session.commit()
+        return user.serialize()
+    
+    except Exception as e:
+        if User.query.filter_by(username = data['username']) is None:
+            raise Exception("User does not exists")
+
+        raise Exception(str(e))
--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from backend.controllers.users import createUser, loginUser
+from backend.controllers.users import createUser, loginUser, getUserByUsername, updateUser
+from flask_jwt_extended import jwt_optional, jwt_required, get_jwt_identity, verify_jwt_in_request
 
 users = Blueprint('users', __name__)
 
@@ -28,9 +29,28 @@ def login():
 #Get current user
 @users.route('/api/user', methods=('GET',))
 def get_user():
-    return "From the get user page"
+    try:
+        # Check if header is in correct format and the token recieved is valid 
+        verification = verify_jwt_in_request()
+        # Access the identity of the current user with get_jwt_identity
+        identity = get_jwt_identity()
+        user = getUserByUsername(identity)
+        return jsonify(user), 200
+    except Exception as e:
+        return jsonify({"errors":{"body": [str(e)]}}), 422
 
-#Update current user
-@users.route('/api/user', methods=('PUT',))
+#Update current user PATCH
+@users.route('/api/user', methods=('PATCH',))
 def update_user():
-    return "From the update user page"
+    try:
+        data = request.get_json()
+        # Check if header is in correct format and the token recieved is valid 
+        # or else throw exception
+        verification = verify_jwt_in_request()
+        # Access the identity of the current user with get_jwt_identity
+        identity = get_jwt_identity()
+        user = updateUser(identity, data['user'])
+        return jsonify(user), 200
+
+    except Exception as e:
+        return jsonify({"errors":{"body": [str(e)]}}), 422
